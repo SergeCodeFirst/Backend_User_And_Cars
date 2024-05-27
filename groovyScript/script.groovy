70b75d2023b3fc570653128108d0d0f602f131b9
Itserge1 <sergecodefirst@gmail.com>
2024-05-27 15:50:17 -0400

    update script location

---
+++ groovyScript/script.groovy
@@ -0,0 +1,13 @@
+def testApp() {
+    echo "Testing the .net app"
+}
+
+def buildApp() {
+    echo "Building the .net app"
+}
+
+def deployApp() {
+    echo "deploying the .net app"
+}
+
+return this
